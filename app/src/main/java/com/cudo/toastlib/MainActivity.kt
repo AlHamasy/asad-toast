07f3365470c43bb4a0fec18c3881abfcbad73c7e
Asadullah Al Hamasy <asadhamasy@gmail.com>
2021-10-22 10:50:44 +0700

    again

--- app/src/main/java/com/cudo/toastlib/MainActivity.kt
+++ app/src/main/java/com/cudo/toastlib/MainActivity.kt
@@ -2,11 +2,14 @@ package com.cudo.toastlib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cudo.asadtoast.AsadToast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AsadToast.showToast(this, "halo ini adalah toast buatan library sendiri")
     }
 }
